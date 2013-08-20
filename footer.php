61dd792dd06b55ec8302f29ca88bd0c0cd42e366
Stéphane Couture <steph@stephcouture.info>
2013-08-20 11:53:10 -0400

    adjust footer

--- footer.php
+++ footer.php
@@ -14,9 +14,13 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'mog_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'mog' ); ?>" rel="generator"><?php printf( __( 'running on %s', 'mog' ), 'WordPress' ); ?></a>
+			À l'exception des textes diffusés dans des revues tiers (voir page "publications", le contenu de ce site est est placé sous une licence 
+			Creative Commons <a href = "http://creativecommons.org/licenses/by-nc-sa/2.5/ca/deed.fr">Paternité, 
+			Pas d’Utilisation Commerciale, Partage dans les mêmes conditions</a>. Pour les droits 
+			d'utilisation des publications, veuillez vous référer à la version officielle.<br>
+			<?php printf( __( 'Fonctionne avec %1$s.'), '<a href="http://wordpress.org/">WordPress</a>'); ?>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'mog' ), 'Mog', '<a href="http://hndr.me/" rel="designer">hndr</a>' ); ?>
+			<?php printf( __( 'Thème: %1$s.'), '<a href="https://github.com/stephcouture/WP_Mog" rel="designer">Mog personnalisé</a>'); ?>			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
